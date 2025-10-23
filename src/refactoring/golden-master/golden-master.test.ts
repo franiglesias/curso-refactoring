e3c09df72603cb681f444f4a06685bf0aecc1ddb
Fran Iglesias <franiglesias@mac.com>
2025-10-23 15:26:02 +0200

    Alternative Golden Master technique

--- src/refactoring/golden-master/golden-master.test.ts
+++ src/refactoring/golden-master/golden-master.test.ts
@@ -2,6 +2,19 @@ import {describe, expect, it} from 'vitest'
 import {generateOrder, ReceiptPrinter} from './golden-master'
 
 describe('Receipt Printer', () => {
+  let counter = 0
+  describe.each(['Ana', 'Luis', 'Mar', 'Iván', 'Sofía'])('Given a customer %s', (customer) => {
+    describe.each([1, 2, 3, 4, 5])('Given a number of items %d', (item) => {
+      describe.each([1, 3, 10, 25, 300])('Given quantity %d', (quantity) => {
+        it('should print a receipt', () => {
+          counter = counter + 1
+          const pedido = generateOrder('ORD-' + counter.toString(), customer, item, quantity)
+          expect(new ReceiptPrinter().print(pedido)).matchSnapshot()
+        })
+      })
+    })
+  })
+
   it('should generate a golden master', () => {
     const printer = new ReceiptPrinter()
 
