4a02749cd2661e9b76efe33986b109a295731f9f
Fran Iglesias <franiglesias@mac.com>
2025-10-30 16:32:20 +0100

    Format changes

--- src/code-smells/bloaters/data-clump.ts
+++ src/code-smells/bloaters/data-clump.ts
@@ -1,8 +1,8 @@
 export class Invoice {
-  private readonly customerName: string;
-  private readonly customerCity: string;
-  private readonly customerStreet: string;
-  private readonly customerZip: string;
+  private readonly customerName: string
+  private readonly customerCity: string
+  private readonly customerStreet: string
+  private readonly customerZip: string
 
   constructor(
     customerName: string,
@@ -10,23 +10,25 @@ export class Invoice {
     customerCity: string,
     customerZip: string,
   ) {
-    this.customerZip = customerZip;
-    this.customerStreet = customerStreet;
-    this.customerCity = customerCity;
-    this.customerName = customerName;
+    this.customerZip = customerZip
+    this.customerStreet = customerStreet
+    this.customerCity = customerCity
+    this.customerName = customerName
   }
 
   print(): string {
-    return `Factura para: ${this.customerName}\n` +
+    return (
+      `Factura para: ${this.customerName}\n` +
       `Dirección: ${this.customerStreet}, ${this.customerCity}, ${this.customerZip}`
+    )
   }
 }
 
 export class ShippingLabel {
-  private readonly customerName: string;
-  private readonly customerStreet: string;
-  private readonly customerCity: string;
-  private readonly customerZip: string;
+  private readonly customerName: string
+  private readonly customerStreet: string
+  private readonly customerCity: string
+  private readonly customerZip: string
 
   constructor(
     customerName: string,
@@ -34,13 +36,16 @@ export class ShippingLabel {
     customerCity: string,
     customerZip: string,
   ) {
-    this.customerZip = customerZip;
-    this.customerCity = customerCity;
-    this.customerStreet = customerStreet;
-    this.customerName = customerName;
+    this.customerZip = customerZip
+    this.customerCity = customerCity
+    this.customerStreet = customerStreet
+    this.customerName = customerName
   }
 
   print(): string {
-    return `Enviar a: ${this.customerName}\n` + `${this.customerStreet}, ${this.customerCity}, ${this.customerZip}`
+    return (
+      `Enviar a: ${this.customerName}\n` +
+      `${this.customerStreet}, ${this.customerCity}, ${this.customerZip}`
+    )
   }
 }
